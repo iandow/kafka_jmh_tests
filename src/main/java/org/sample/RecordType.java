81a2fe716e7a2de0f6ee38b8283e3621c3f8e7d3
Ian Downard <kayaking@gmail.com>
2016-11-15 09:04:20 -0800

    first commit

---
+++ src/main/java/org/sample/RecordType.java
@@ -0,0 +1,28 @@
+package org.sample;
+
+import java.io.Serializable;
+
+public class RecordType implements Serializable {
+    private String str1;
+    private String str2;
+    private int int1;
+
+    public String getStr1() {
+        return str1;
+    }
+    public void setStr1(String str1) {
+        this.str1 = str1;
+    }
+    public String getStr2() {
+        return str2;
+    }
+    public void setStr2(String str2) {
+        this.str2 = str2;
+    }
+    public int getInt1() {
+        return int1;
+    }
+    public void setInt1(int int1) {
+        this.int1 = int1;
+    }
+}
